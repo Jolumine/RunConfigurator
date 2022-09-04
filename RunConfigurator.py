0dc0eed482db101d2ff734d4968bbea6c668abe1
Leonard Becker <beckerleonard911@gmail.com>
2022-09-04 15:38:04 +0200

    Update RunConfigurator.py
    This commit added the functionality for command-line arguments.

--- RunConfigurator.py
+++ RunConfigurator.py
@@ -6,20 +6,21 @@ import sys
 
 class RunConfigurator:
     def __init__(self, arguments) -> None:
-        self.config_file = "./config.json"
+        self.config_file = "./configs.json"
         if len(arguments) > 1: 
             self.script = arguments[1].split("-")[1]
             self.execute()
         else: 
             print("Error, no configuration specified.")
         
+    # Main Method 
     def execute(self):
-        if self.script == "init" and not self.check_file(self.config_file):
+        if self.script == "init" and not self.check_file(self.config_file): # inits the whole application 
             print("Initilization")
             with open(self.config_file, "w") as f: 
                 f.write("{}")
         
-        elif self.script == "add" and self.check_file(self.config_file):
+        elif self.script == "add" and self.check_file(self.config_file): # creates new script / configuration
             print("Add configuration: ")
             name = input("Name: ")
             path = input("Script Path (Enter absolute path): ")
@@ -32,20 +33,31 @@ class RunConfigurator:
             with open(self.config_file, "w") as f: 
                 json.dump(parsed, f, indent=4, sort_keys=False)
         
-        else: 
+        else: # Executes the script
             if self.check_file(self.config_file):
                 with open(self.config_file, "r") as f: 
                     parsed = json.load(f)
                 if self.check_config(self.script, parsed):
                     file = parsed[self.script]["file"]
                     folder = "".join(os.listdir("."))
-                    if "venv" in folder:
+
+                    if "env" in folder:
+                        if parsed[self.script]["args"] != "":
+                            args = parsed[self.script]["args"].split(" ")
+                            subprocess.call(["python", file] + args)
+                        else: 
+                            pass  
                         for element in os.listdir("."):
-                            if "venv" in element: 
+                            if "env" in element: 
                                 python = f"./{element}/Scripts/python.exe"
                                 subprocess.call([python, file])
+
                     else: 
-                        subprocess.call(["python", file])
+                        if parsed[self.script]["args"] != "":
+                            args = parsed[self.script]["args"].split(" ")
+                            subprocess.call(["python", file] + args)
+                        else: 
+                            subprocess.call(["python", file])
                 else: 
                     print("Error, command not defined.")
             else: 
